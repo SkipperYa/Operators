42b8211f08df41d3b2701c18498ecc3608b6b314
Stanislav <skipper3191@yandex.ru>
2024-08-24 13:34:21 +0300

    remove useless files and add operator controller

---
+++ Operators/Operators/Operators.Server/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Operators.Server.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class BaseController : ControllerBase
+	{
+	}
+}
---
+++ Operators/Operators/Operators.Server/Controllers/Operators/OperatorsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Operators.Server.Entities.Operators;
+
+namespace Operators.Server.Controllers.Operators
+{
+	public class OperatorsController : BaseController
+	{
+		public OperatorsController()
+		{
+
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetList()
+		{
+			return Ok();
+		}
+
+		[HttpGet("{id}")]
+		public async Task<IActionResult> GetOperator(long id)
+		{
+			return Ok();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateOperator([FromBody] Operator operatorModel)
+		{
+			return Ok();
+		}
+
+		[HttpPut]
+		public async Task<IActionResult> UpdateOperator([FromBody] Operator operatorModel)
+		{
+			return Ok();
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteOperator(long id)
+		{
+			return Ok();
+		}
+	}
+}
