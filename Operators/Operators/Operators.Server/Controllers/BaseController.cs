c6d9eb9d6149c8945cb04244aeaf3fe3fc7398a4
Stanislav <skipper3191@yandex.ru>
2024-08-24 15:52:48 +0300

    add timout, compression and cache response

--- Operators/Operators/Operators.Server/Controllers/BaseController.cs
+++ Operators/Operators/Operators.Server/Controllers/BaseController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.Timeouts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Operators.Server.Controllers
 {
 	[ApiController]
 	[Route("[controller]")]
+	[RequestTimeout("DefaultTimeout10s")]
 	public class BaseController : ControllerBase
 	{
+
 	}
 }
--- Operators/Operators/Operators.Server/Program.cs
+++ Operators/Operators/Operators.Server/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.AspNetCore.Http.Timeouts;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
+using Operators.Server.Entities.ControllerFilters;
 using Operators.Server.Entities.Database;
 using Operators.Server.Entities.Extensions;
+using Operators.Server.Interfaces;
+using Operators.Server.Services;
+using System.IO.Compression;
 
 namespace Operators.Server
 {
@@ -10,9 +16,36 @@ namespace Operators.Server
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
+			builder.Logging.ClearProviders();
+			builder.Logging.AddConsole();
+
 			// Add services to the container.
 
-			builder.Services.AddControllers();
+			builder.Services.AddResponseCaching();
+			builder.Services.AddResponseCompression(options =>
+			{
+				options.EnableForHttps = true;
+				options.Providers.Add<BrotliCompressionProvider>();
+			});
+
+			builder.Services.AddRequestTimeouts(options =>
+			{
+				options.AddPolicy("DefaultTimeout10s", new RequestTimeoutPolicy()
+				{
+					Timeout = TimeSpan.FromMilliseconds(10000),
+				});
+			});
+
+			builder.Services.Configure<BrotliCompressionProviderOptions>(options =>
+			{
+				options.Level = CompressionLevel.Optimal;
+			});
+
+			builder.Services.AddControllers(options =>
+			{
+				options.Filters.Add(typeof(GlobalExceptionFilter));
+			});
+
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
@@ -20,6 +53,9 @@ namespace Operators.Server
 			var connection = builder.Configuration.GetConnectionString("DefaultConnection");
 
 			builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection), ServiceLifetime.Transient, ServiceLifetime.Transient);
+
+			builder.Services.AddTransient<IOperatorsRepositoryService, OperatorsRepositoryService>();
+			builder.Services.AddTransient<IOperatorValidatorService, OperatorValidatorService>();
 
 			var app = builder.Build();
 
@@ -33,6 +69,8 @@ namespace Operators.Server
 			app.UseHttpsRedirection();
 
 			app.MapControllers();
+
+			app.UseResponseCompression();
 
 			app.MapFallbackToFile("/index.html");
 
